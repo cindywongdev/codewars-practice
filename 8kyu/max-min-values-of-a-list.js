7cf60bc037a41d752688f826d8ce3348e160cc2b
Cindy Wong <cindywongdev@gmail.com>
2023-01-10 19:06:20 -0500

    started max-min kata

--- 8kyu/max-min-values-of-a-list.js
+++ 8kyu/max-min-values-of-a-list.js
@@ -1,5 +1,5 @@
 // DESCRIPTION:
-// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
 // Examples (Input -> Output)
 // * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
@@ -9,5 +9,17 @@
 // Notes
 // You may consider that there will not be any empty arrays/vectors.
 
+const array = [4,6,2,1,9,63,-134,566]
 
 // My Solution
+
+const findMax = (array) => {
+    return "This is max"
+}
+
+const findMin = (array) => {
+    return "This is min"
+}
+
+console.log(findMax(array))
+console.log(findMin(array))
