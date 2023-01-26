9daa19b9ccf4270d476876521db620b3ef06161a
Cindy Wong <cindywongdev@gmail.com>
2023-01-25 19:39:32 -0500

    completed kata

--- 7kyu/sum-of-odd-numbers.js
+++ 7kyu/sum-of-odd-numbers.js
@@ -8,9 +8,11 @@
 // ...
 // Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 
-// 1 -->  1
-// 2 --> 3 + 5 = 8
+// 1 -->  1 = 1**3
+// 2 --> 3 + 5 = 8 = 2**3
+// 3 --> 7 + 9 + 11 = 27 = 3**3
+// 4 --> 13 + 15 + 17 + 19 = 4**3
 
 function rowSumOddNumbers(n) {
-	// TODO
+	return n**3
 }
