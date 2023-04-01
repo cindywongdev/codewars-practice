e067ecf7eca39699cfb64b8b15f10929aca1cedb
Cindy Wong <cindywongdev@gmail.com>
2023-04-01 15:54:53 -0400

    starting alt solution

--- 6kyu/roman-numerals-encoder.js
+++ 6kyu/roman-numerals-encoder.js
@@ -28,7 +28,7 @@ const min = 1
 const max = 3999
 
 // const number = 1666
-const number = 3999
+const number = 166
 
 // ALT SOLUTION IDEA!!!
 // initialize result array
@@ -40,95 +40,118 @@ const number = 3999
 // if # is a 0, skip it
 // if end of array is reached, return the result!
 
-function solution(number) {
-    // convert the number to a roman numeral
-    // define constants
-    const M = 1000
-    const D = 500
-    const C = 100
-    const L = 50
-    const X = 10
-    const V = 5
-    const I = 1
-
-    // initialize empty array and remainder var
+function solution(number){
+    // initialize result array
     const result = []
-    let remainder = number
 
-    // find any and all 9's first (special treatment)
-    let indicesOf9 = []
+    // split number into array
     const numToStrArr = number.toString().split("")
-    let indexOf9 = numToStrArr.indexOf("9")
-    while (indexOf9 !== -1) {
-        indicesOf9.push(indexOf9)
-        indexOf9 = numToStrArr.indexOf("9", indexOf9 + 1)
-    }
-    console.log(indicesOf9)
 
-    // function to check value of 9 based on index (only needa search indexes 1,2,3 since max val is 3999)
-    function convert9(index){
-        // if 9 is in the hundreds place (900)
-        if (index === 1) {
-            return "CM"
-        }
-        // if 9 is in the tens place (90)
-        if (index === 2) {
-            return "XC"
-        }
-        // if 9 is in the ones place (9)
-        if (index === 3) {
-            return "IX"
-        }
+    let arrLength = numToStrArr.length
+
+    // if length is < 4 (number is smaller than 1000, add 0's in front so that all num arrays are same length)
+    while (arrLength < 4) {
+        numToStrArr.unshift('0')
+        arrLength += 1
     }
 
-    let LetterArray9 = []
+    // now iterate through array
+    // for (num of numToStrArr) {
 
-    for (index of indicesOf9) {
-        LetterArray9.push(convert9(index))
-    }
+    // }
 
-    console.log(LetterArray9)
-
-    function findNumberOfLetter(remainder, letterVal) {
-        const numOfLetter = Math.floor(remainder / letterVal)
-        return numOfLetter
-    }
-
-    function addToResult(numOfLetter, letterStr) {
-        for (let i = 0; i < numOfLetter; i += 1) {
-            result.push(letterStr)
-        }
-    }
-
-    function getRemainder(remainder, numOfLetter, letterVal) {
-        remainder = remainder - (numOfLetter * letterVal)
-        return remainder
-    }
-
-    // function to find # of one letter in a number
-    function determineNumLetter(remainder, letterVal, letterStr) {
-        const numOfLetter = findNumberOfLetter(remainder, letterVal)
-        addToResult(numOfLetter, letterStr)
-        remainder = getRemainder(remainder, numOfLetter,
-            letterVal)
-        console.log("remainder in function", remainder)
-        return remainder
-    }
-
-    while (remainder > 0) {
-        remainder = determineNumLetter(number, M, "M")
-        remainder = determineNumLetter(remainder, D, "D")
-        remainder = determineNumLetter(remainder, C, "C")
-        remainder = determineNumLetter(remainder, L, "L")
-        remainder = determineNumLetter(remainder, X, "X")
-        remainder = determineNumLetter(remainder, V, "V")
-        remainder = determineNumLetter(remainder, I, "I")
-        
-    }
-
-    // turn result array into str
-    return result.join("")
+    return(numToStrArr)
 }
+
+// function solution(number) {
+//     // convert the number to a roman numeral
+//     // define constants
+//     const M = 1000
+//     const D = 500
+//     const C = 100
+//     const L = 50
+//     const X = 10
+//     const V = 5
+//     const I = 1
+
+//     // initialize empty array and remainder var
+//     const result = []
+//     let remainder = number
+
+//     // find any and all 9's first (special treatment)
+//     let indicesOf9 = []
+//     const numToStrArr = number.toString().split("")
+//     let indexOf9 = numToStrArr.indexOf("9")
+//     while (indexOf9 !== -1) {
+//         indicesOf9.push(indexOf9)
+//         indexOf9 = numToStrArr.indexOf("9", indexOf9 + 1)
+//     }
+//     console.log(indicesOf9)
+
+//     // function to check value of 9 based on index (only needa search indexes 1,2,3 since max val is 3999)
+//     function convert9(index){
+//         // if 9 is in the hundreds place (900)
+//         if (index === 1) {
+//             return "CM"
+//         }
+//         // if 9 is in the tens place (90)
+//         if (index === 2) {
+//             return "XC"
+//         }
+//         // if 9 is in the ones place (9)
+//         if (index === 3) {
+//             return "IX"
+//         }
+//     }
+
+//     let LetterArray9 = []
+
+//     for (index of indicesOf9) {
+//         LetterArray9.push(convert9(index))
+//     }
+
+//     console.log(LetterArray9)
+
+//     function findNumberOfLetter(remainder, letterVal) {
+//         const numOfLetter = Math.floor(remainder / letterVal)
+//         return numOfLetter
+//     }
+
+//     function addToResult(numOfLetter, letterStr) {
+//         for (let i = 0; i < numOfLetter; i += 1) {
+//             result.push(letterStr)
+//         }
+//     }
+
+//     function getRemainder(remainder, numOfLetter, letterVal) {
+//         remainder = remainder - (numOfLetter * letterVal)
+//         return remainder
+//     }
+
+//     // function to find # of one letter in a number
+//     function determineNumLetter(remainder, letterVal, letterStr) {
+//         const numOfLetter = findNumberOfLetter(remainder, letterVal)
+//         addToResult(numOfLetter, letterStr)
+//         remainder = getRemainder(remainder, numOfLetter,
+//             letterVal)
+//         console.log("remainder in function", remainder)
+//         return remainder
+//     }
+
+//     while (remainder > 0) {
+//         remainder = determineNumLetter(number, M, "M")
+//         remainder = determineNumLetter(remainder, D, "D")
+//         remainder = determineNumLetter(remainder, C, "C")
+//         remainder = determineNumLetter(remainder, L, "L")
+//         remainder = determineNumLetter(remainder, X, "X")
+//         remainder = determineNumLetter(remainder, V, "V")
+//         remainder = determineNumLetter(remainder, I, "I")
+        
+//     }
+
+//     // turn result array into str
+//     return result.join("")
+// }
 
 console.log(solution(number))
 
