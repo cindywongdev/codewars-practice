99fb9d84fa336cb73e99a07702131c331377ba82
Cindy Wong <cindywongdev@gmail.com>
2023-04-12 10:35:11 -0400

    started deadfish

---
+++ 6kyu/make-the-deadfish-swim.js
@@ -0,0 +1,16 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+
+function parse(data){
+
+}
+
+parse("iiisdoso") // => [ 8, 64 ]
