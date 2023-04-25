7f62bb3d5430884e26a2c3d9a1ef0256f8ae196e
Cindy Wong <cindywongdev@gmail.com>
2023-04-24 22:37:07 -0400

    added function skeleton

--- 6kyu/count-the-smiley-faces.js
+++ 6kyu/count-the-smiley-faces.js
@@ -16,3 +16,7 @@
 // countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 // Note
 // In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+function countSmileys(arr) {
+    return 0;
+}
